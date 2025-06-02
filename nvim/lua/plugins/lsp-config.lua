2e18d64d51da00f266780bb1c28d92fc64906784
ssuarezs <suarez.santiago.113@gmail.com>
2025-06-02 17:19:43 -0500

    none-ls setup

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -15,9 +15,14 @@ return {
           "omnisharp"
         },
       })
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
+
+      local opts = { noremap=true, silent=true }
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+      vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+      vim.keymap.set({ 'n' }, '<leader>rn', vim.lsp.buf.rename, opts)
     end
   }
 }
---
+++ nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,32 @@
+return {
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		local formatting = null_ls.builtins.formatting
+
+		local eslint_d = require("none-ls.diagnostics.eslint_d")
+		local flake8 = require("none-ls.diagnostics.flake8")
+
+		null_ls.setup({
+			sources = {
+				-- lua
+				formatting.stylua,
+
+				-- python
+				formatting.black.with({ extra_args = { "--fast" } }),
+        formatting.isort,
+				flake8,
+
+				-- Typescript / Javascript
+				formatting.prettier,
+				eslint_d,
+			},
+		})
+
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
+}
