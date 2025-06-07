9a9fe7677d32ab3786ff3e96c857e1ac14198aec
ssuarezs <suarez.santiago.113@gmail.com>
2025-06-07 00:31:14 -0500

    Nvim autocomplete and snippets

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -3,10 +3,19 @@ return {
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", 
+    "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal right<CR>')
-  end
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true, -- This shows hidden files (dotfiles)
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+    })
+    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
+  end,
 }
